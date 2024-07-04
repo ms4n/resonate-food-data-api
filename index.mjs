44ab21385737f74b6b6506a753bc5a2424ac0468
sanjay <arjunsanjay0@gmail.com>
2024-07-04 15:08:40 +0530

    remove logic to convert macros per 100gm

--- index.mjs
+++ index.mjs
@@ -1,11 +1,9 @@
 import puppeteer from "puppeteer";
 import "dotenv/config";
 
-async function fetchNutritionalData(foodSlug) {
+async function fetchNutritionalData(foodItem) {
   const nutritionalDataUrl = process.env.NUTRITIONAL_DATA_URL;
-  const url = `${nutritionalDataUrl}${foodSlug}`;
-
-  console.log(url);
+  const url = `${nutritionalDataUrl}${foodItem}`;
 
   const browser = await puppeteer.launch({
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
@@ -14,11 +12,9 @@ async function fetchNutritionalData(foodSlug) {
 
   try {
     await page.goto(url, { waitUntil: "domcontentloaded" });
-    console.log("Fetched URL!");
 
     // wait for the nutrition info to be present
     await page.waitForSelector(".nf", { timeout: 10000 });
-    console.log("Nutrition data div is present!");
 
     // extract text content
     async function extractText(selector, useXPath = false) {
@@ -62,41 +58,19 @@ async function fetchNutritionalData(foodSlug) {
     //extract all the required macros
     const macrosData = {};
     for (const key in macrosSelectors) {
-      macrosData[key] = await extractText(
-        macrosSelectors[key],
-        key.includes("total_") || key === "dietary_fiber" || key === "protein"
-      );
-    }
+      const useXPath =
+        key.includes("total_") || key === "dietary_fiber" || key === "protein";
+      const valueText = await extractText(macrosSelectors[key], useXPath);
 
-    console.log("Extracting information finished");
-
-    // extract serving size
-    const singleServingWeight = parseFloat(
-      macrosData.single_serving_size.split("(")[1].split("g")[0]
-    );
-    const ratioPer100g = 100 / singleServingWeight;
-
-    // calculate values per 100 grams
-    const macros100g = {};
-    for (const key in macrosData) {
-      if (key !== "single_serving_size") {
-        macros100g[`${key}_100g`] =
-          Math.round(
-            parseFloat(macrosData[key].split("g")[0]) * ratioPer100g * 10
-          ) / 10;
-        macros100g.calories_per_serving = parseFloat(macrosData.calories);
+      if (key === "single_serving_size") {
+        const servingSize = parseFloat(valueText.split("(")[1].split("g")[0]);
+        macrosData[key] = servingSize;
       } else {
-        try {
-          macros100g[key] = parseFloat(macrosData[key]);
-        } catch (e) {
-          macros100g[key] = macrosData[key];
-        }
+        macrosData[key] = parseFloat(valueText);
       }
     }
 
-    macros100g.single_serving_size = singleServingWeight;
-
-    return macros100g;
+    return macrosData;
   } catch (error) {
     console.error("Error fetching nutritional data:", error);
   } finally {
@@ -104,6 +78,10 @@ async function fetchNutritionalData(foodSlug) {
   }
 }
 
-fetchNutritionalData("egg")
+async function calculateMacroData(foodItem, count, weight) {
+ 
+}
+
+fetchNutritionalData("chicken-breast")
   .then((data) => console.log("Nutritional data fetched and stored:", data))
   .catch((err) => console.error(err));
